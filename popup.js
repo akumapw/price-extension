fec3fe4f26b2e78d5aedf66029f3e871e64a7afe
akumapw <lucasrodrigues13523@gmail.com>
2025-08-11 15:26:48 -0300

    importando variáveis

---
+++ popup.js
@@ -0,0 +1,10 @@
+const urlInput = document.getElementById('urlInput');
+const pasteBtn = document.getElementById('pasteBtn');
+const folderSelect = document.getElementById('folderSelect');
+const newFolderBtn = document.getElementById('newFolderBtn');
+const newFolderWrap = document.getElementByI('newFolderWrap');
+const newFolderInput = document.getElementById('newFolderInput');
+const createFolderConfirm = document.getElementById('createFolderConfirm');
+const saveBtn = document.getElementById('saveBtn');
+const openOptionsBtn = document.getElementById('openOptionsBtn');
+
