82455fbde7bf2eac75e52bb82685a87c32cdbcf2
akumapw <lucasrodrigues13523@gmail.com>
2025-08-15 17:17:28 -0300

    importação das váriaveis

---
+++ options.js
@@ -0,0 +1,7 @@
+const foldersWrap = document.getElementById('foldersWrap');
+const folderName = document.getElementById('folderName');
+const addFolderBtn = document.getElementById('addFolderBtn');
+const exportBtn = document.getElementById('exportBtn');
+const importBtn = document.getElementById('importBtn');
+const importFile = document.getElementById('importFile');
+
